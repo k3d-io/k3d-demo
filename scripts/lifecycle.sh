83eab014444e6c604d760607d66863750cba76ee
Thorsten Klein <iwilltry42@gmail.com>
2021-02-05 18:51:02 +0100

    adjust steps, add comments and update ingress spec

--- scripts/lifecycle.sh
+++ scripts/lifecycle.sh
@@ -20,7 +20,7 @@ section "Access the Cluster"
 
 info_pause_exec "List clusters" "k3d cluster list"
 
-info_pause_exec "Update the default kubeconfig with the new cluster details" "k3d kubeconfig merge demo --kubeconfig-merge-default --kubeconfig-switch-context"
+info_pause_exec "Update the default kubeconfig with the new cluster details (Optional, included in 'k3d cluster create' by default)" "k3d kubeconfig merge demo --kubeconfig-merge-default --kubeconfig-switch-context"
 # info "Cluster Name: demo"
 # info "--merge-default-kubeconfig true: overwrite existing fields with the same name in kubeconfig (true by default)"
 # info "--kubeconfig-switch-context true: set the kubeconfig's current-context to the new cluster context (false by default)"
@@ -38,13 +38,13 @@ info_pause_exec "Create a new 'demo' namespace" "kubectl create namespace demo"
 info_pause_exec "Switch to the new 'demo' namespace" "kubens demo"
 info_pause_exec "Deploy the sample app with helm" "helm upgrade --install sample-app sample/conf/charts/sample-app --namespace demo --set app.image=sample-app:local"
 
-info_pause_exec "Access the sample app frontend via ingress" "chromium --new-window http://sample.k3d.localhost:8080 &>/dev/null &"
+info_pause_exec 'Access the sample app frontend via ingress (this uses Chromium): You can change some things in the code while having the window open and see everything hot reload, when you refresh the site!' "chromium --new-window http://sample.k3d.localhost:8080 &>/dev/null &"
 
 section "Grow the Cluster"
 
 info_pause_exec "Add 2 agents to the cluster" "k3d node create new-agent --cluster demo --role agent --replicas 2"
 
-info_pause_exec "Use kubectl to see the new nodes" "kubectl get nodes"
+info_pause_exec '(Wait a bit for the nodes to get ready!) Use kubectl to see the new nodes' "kubectl get nodes"
 
 section "Start/Stop the Cluster"
 
